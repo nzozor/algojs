fc46288da8734dab62292536ca1f571136f5c0b9
Gitea <gitea@fake.local>
2019-11-19 16:53:45 +0000

    multiple solution reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverseBak(str) {
+    const reversedStr = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr.push(str[i]);
+    }
+    return reversedStr.join('');
+}
+
+function reverse2(str) {
+    return str.split('').reverse().join('');
+} 
+
+function reverse3(str) {
+    
+    let reversedStr = '';
+    for (const char of str) {
+        reversedStr = char + reversedStr;
+    }
+    return reversedStr;
+} 
+
+function reverse(str) {
+    return str.split('').reduce((result, char) => char + result, '');
+}
+
+console.log(reverse("abcd"));
+console.log(reverse2("abcd"));
+console.log(reverse3("abcd"));
+
 
 module.exports = reverse;
