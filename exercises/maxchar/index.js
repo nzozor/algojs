ae99615268caf77a3212ef20dc920ff48150f5e4
Gitea <gitea@fake.local>
2019-11-22 18:11:51 +0000

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const hashTable = {};
+    for( char of str) {
+        if (hashTable[char]) {
+            ++hashTable[char];
+        } else {
+            hashTable[char] = 1;
+        }
+    }
+    let maxVal = -1;
+    let maxKey = -1;
+    console.log(hashTable);
+    for(const [key, value] of Object.entries(hashTable)) {
+        if(value > maxVal) {
+            maxKey = key;
+            maxVal = value;
+        }
+    }
+    return maxKey;
+}
+
+console.log(maxChar('abcdefghijklmnaaaaa'));
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let sign = Math.sign(n);
+    return parseInt(Math.abs(n).toString(10).split('').reduce( (result, item) => item + result , ''), 10) * sign;   
+}
 
+function reverseInt2(n) {
+    let sign;
+    if(n !== 0) {
+        sign = Math.sign(n) == 1 ? '+' : '-' ;
+    } else {
+        sign = '';
+    }
+
+    return parseInt(sign + Math.abs(n).toString(10).split('').reduce( (result, item) => item + result , ''), 10);   
+}
+
+console.log(reverseInt(-981)); 
 module.exports = reverseInt;
