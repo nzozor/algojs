f0d2a5daeac5d64abad36990805b169ccaf9e9d1
nick.z <nzozor@gmail.com>
2019-11-24 20:08:06 +0000

    anagrams

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,53 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+function chunk(array, size) {
+    const chunked = [];
+
+    for (const el of array) {
+        const last = chunked[chunked.length -1];
+        /*jshint -W087 */
+        debugger;
+        if (!last || last.length === size ) {
+            chunked.push([el]);
+        } else {
+            last.push(el);
+        }
+    }
+    return chunked;
+}
+
+function chunk3(array, size) {
+    let index = 0;
+    const chunked = [];
+    
+    while(index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    return chunked;
+}
+
+function chunk2(array, size) {
+    const arrayOfChunks = [];
+    let chunk = [];
+    for (const el of array) {
+        if(chunk.length < size) {
+
+            chunk.push(el);
+        } else {
+            arrayOfChunks.push(chunk);
+            chunk = [el];
+        }
+    }
+    if (chunk.length > 0) {
+        arrayOfChunks.push(chunk);
+    }
+    return arrayOfChunks;
+}
+console.log(chunk([1, 2, 3, 4, 5], 2));
+console.log(chunk([1, 2, 3, 4, 5], 10));
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
 
 module.exports = chunk;
