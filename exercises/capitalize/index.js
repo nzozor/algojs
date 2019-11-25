52cc4ffe47c67f70b311e13175f3bd7951018866
Gitea <gitea@fake.local>
2019-11-25 18:20:02 +0000

    steps

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    return str.split(' ').map((word) => { 
+        return word.split('').map((letter, idx) => {
+            return idx === 0 ? letter.toUpperCase() : letter;
+        }).join('');
+     }).join(' ');
+}
+
+
+function capitalise2(str) {
+    capStr = str[0].toUpperCase();
+    str = str.slice(1).split('');
+    for(const [index, char] of str.entries()) {
+        if (str[index - 1] === ' ') {
+            capStr += char.toUpperCase();
+        } else {
+            capStr += char;
+        }
+    }
+    return capStr;
+}
+
 
 module.exports = capitalize;
